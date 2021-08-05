7bbc3a1828ab914ebd02896697807819c56fe7bc
SunilP <spottumuttu@osius.com>
2021-08-05 15:41:58 +0530

    initial version

---
+++ CloudKarafka.Consumer.Net/Program.cs
@@ -0,0 +1,73 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace CloudKarafka.Consumer.Net
+{
+    class Program
+    {
+        public static IConfigurationRoot configuration;
+        static async Task Main(string[] args)
+        {
+            configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+            .AddJsonFile("appsettings.json", false)
+            .Build();
+
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
+
+            var bootstrapServers = configuration["BootstrapServers"].ToString();
+            var username = configuration["UserName"].ToString();
+            var password = configuration["Password"].ToString();
+            var topicPrifix = configuration["TopicPrefix"].ToString();
+            var topic = $"{topicPrifix}default";
+
+            var config = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = bootstrapServers,
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SaslMechanism = SaslMechanism.ScramSha256,
+                SaslUsername = username,
+                SaslPassword = password,
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+            };
+            using var c = new ConsumerBuilder<Ignore, string>(config).Build();
+            c.Subscribe(topic);
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            try
+            {
+                for (; ; )
+                {
+                    try
+                    {
+                        var cr = await Task.FromResult(c.Consume(cts.Token));
+                        Console.WriteLine($"Consumed message '{cr.Message.Value}' at: '{cr.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occurred: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                c.Close();
+            }
+        }
+    }
+}
---
+++ CloudKarafka.Producer.Net/Program.cs
@@ -0,0 +1,54 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CloudKarafka.Producer.Net
+{
+    class Program
+    {
+        public static IConfigurationRoot configuration;
+        static async Task Main(string[] args)
+        {
+            configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+            .AddJsonFile("appsettings.json", false)
+            .Build();
+
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
+
+            var bootstrapServers = configuration["BootstrapServers"].ToString();
+            var username = configuration["UserName"].ToString();
+            var password = configuration["Password"].ToString();
+            var topicPrifix = configuration["TopicPrefix"].ToString();
+            var topic = $"{topicPrifix}default";
+
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = bootstrapServers,
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SaslMechanism = SaslMechanism.ScramSha256,
+                SaslUsername = username,
+                SaslPassword = password,
+            };
+
+            using var p = new ProducerBuilder<Null, string>(config).Build();
+            try
+            {
+                var dr = await p.ProduceAsync($"{topic}", new Message<Null, string>
+                {
+                    //for demo purpose just posting topic value as datetime.now
+                    Value = $"topic posted on {DateTime.Now}"
+                });
+                Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+            }
+            catch (ProduceException<Null, string> e)
+            {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+            }
+        }
+    }
+}
